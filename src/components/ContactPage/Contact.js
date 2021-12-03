86b4f84c043f11e8b789ecae00b7c719f6ab57b1
anurag-k01 <aayandcruzz452@gmail.com>
2021-12-03 11:16:09 +0530

    Working on Contact Page

--- src/components/ContactPage/Contact.js
+++ src/components/ContactPage/Contact.js
@@ -23,10 +23,84 @@ function Contact() {
         </div>
 
         <div className="layout">
-          <div>
-            <h2>Reach Out via Socials</h2>
+          <div className="reachOut_div">
+            <h2 className="reachOut">Reach Out via Socials</h2>
+
+            <div className="flex items-center">
+              <div className="flex flex-col text-lg  text-grey">
+                <p>1</p>
+                <p>2</p>
+                <p>3</p>
+                <p>4</p>
+                <p>5</p>
+                <p>6</p>
+              </div>
+              <div className="flex flex-col space-y-2 pl-9">
+                <p className="social">
+                  <span>.socials</span>
+                  {"  {"}
+                </p>
+                <div className="flex flex-col space-y-2 pl-9">
+                  <p className="text-[#857ee2]">
+                    <span className="text-white">email:</span>
+                    <span>
+                      <a
+                        href="mailto:anurag.kmr3214@gmail.com"
+                        style={{
+                          textDecoration: "none",
+                          color: "#857EE2",
+                        }}
+                      >
+                        anurag.kmr3214@gmail.com
+                      </a>
+                    </span>
+                  </p>
+                  <p className="text-[#857ee2]">
+                    <span className="text-white">github:</span>
+                    <span className="text-white">
+                      <a
+                        href="https://github.com/anurag-k01"
+                        style={{
+                          textDecoration: "none",
+                          color: "#857EE2",
+                        }}
+                      >
+                        anurag-k01
+                      </a>
+                    </span>
+                  </p>
+                  <p className="text-[#857ee2]">
+                    <span className="text-white">instagram:</span>
+                    <span className="text-white">
+                      <a
+                        href="https://github.com/anurag-k01"
+                        style={{
+                          textDecoration: "none",
+                          color: "#857EE2",
+                        }}
+                      >
+                        anurag-k01
+                      </a>
+                    </span>
+                  </p>
+                  <p className="text-[#857ee2]">
+                    <span className="text-white">instagram:</span>
+                    <span className="text-white">
+                      <a
+                        href="https://github.com/anurag-k01"
+                        style={{
+                          textDecoration: "none",
+                          color: "#857EE2",
+                        }}
+                      >
+                        anurag-k01
+                      </a>
+                    </span>
+                  </p>
+                </div>
+              </div>
+            </div>
           </div>
-          
         </div>
       </div>
     </>
